6e099109c630cb2074d1f936dd8e26f2826758a9
Kamule <benjamin.herbomez@esial.net>
2012-06-15 21:08:33 +0200

    Add synchro exception error fields

--- application/core/synchronizationException.php
+++ application/core/synchronizationException.php
@@ -2,11 +2,14 @@
 
 class SynchronizationException extends Exception{
     static $msg = null;
+    private $errorFields;
     
-    function __construct(){
+    function __construct($errorFields){
+        //Load error message on the first call
         if(Synchronization_error::$msg == null)
             $this->loadErrorMsg();
         parent::__construct(Synchronization_error::$msg);
+        $this->errorFields = $errorFields;
     }
     
     private function loadErrorMsg(){
@@ -14,6 +17,15 @@ class SynchronizationException extends Exception{
         $CI->load->model('LanguageManager_model', 'languageManager');
         var_dump($this);
         $CI->lang->load($file, $this->languageManager->getLanguage());
+    }
+    
+    /**
+     * Return synchro error fields
+     * 
+     * @return array(field => array('old' => value, 'new' => value)) 
+     */
+    public function getErrorFields(){
+        return $this->errorFields;
     }
 }
 
